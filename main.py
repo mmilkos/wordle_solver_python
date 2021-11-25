21bdf2c02863f623a7891f868fe5eddd568dafc2
Mateusz <84026986+mmilkos@users.noreply.github.com>
2021-11-25 22:44:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 from tkinter import *
 
 root = Tk()
+root.title('Kalkulator')
 
-numbers = []
-
-#funkcje kalkulatora
-
-
-def dodawanie(a, b):
-    return a + b
+first_num = ""
+second_num = ""
+isButtonClicked = False
 
 
-def odejmowanie(a, b):
-    return a - b
+first_holder = []
+second_holder = []
+
+e = Entry(root, width=35, borderwidth=5, state='disabled')
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 
-def mnozenie(a, b):
-    return a * b
+def button_add(but):
+    if not isButtonClicked:
+        first_holder.append(str(but['text']))
+        first_num = ("".join(first_holder))
+        print(first_num)
+    else:
+        second_holder.append(str)
 
 
-def dzielenie(a, b):
-    return a/b
+i = 9
+r = 3
+c = 3
+for row in range(0, r):
+    for col in range(0, c):
+        button = Button(root, text=i, padx="40", pady="20")
+        button.grid(row=row + 2, column=c - col)
+        button['command'] = lambda btn = button: button_add(btn)
+        i -= 1
 
+buttonPlus = Button(root, text='+', padx="39", pady="19")
+buttonPlus.grid(row=row + 3, column=c - col)
+buttonPlus['command'] = lambda x = first_num, y = second_num: print(x + y)
+buttonMinus = Button(root, text='-', padx="39", pady="19")
+buttonMinus.grid(row=row + 3, column=c - col + 1)
+buttonMultiply = Button(root, text='*', padx="39", pady="20")
+buttonMultiply.grid(row=row + 3, column=c - col + 2)
+buttonDivide = Button(root, text='/', padx="39", pady="20")
+buttonDivide.grid(row=row + 4, column=c - col)
 
-i = 1;
-for row in range(0,3):
-    for col in range(0,3):
-        button = Button(root, text= i, padx="5", pady="5").grid(row= row, column= col, command=self.buttonClick)
-        i+=1
+[]
 
-def buttonClick(self):
 
 root.mainloop()
+
